1cd0dcb3aab32cdb4e591ea50f9aa5d85f9209f5
Philip Oliveira <mathphilips007@gmail.com>
2025-11-06 16:25:04 -0300

    feat(java): Adiciona exemplos práticos de Tipos Primitivos

---
+++ PrimitiveTypes.java
@@ -0,0 +1,16 @@
+public class TiposPrimitivos {
+  public static void main(String[] args) {
+    //int, double, float, char, byte, short, long, boolean, String 
+    byte b = 1;
+    short s = 1;
+    int i = 1;
+    long l = 1L;
+    float f = 1.1f;
+    double d = 1.1;
+    String str = "Não.";
+    char c = 'N';
+    boolean bool = true;
+
+    System.out.println("Byte: "+b+", Short: "+s+", Integer: "+i+", Long: "+l+", Float: "+f+", Double: "+d+", String: "+str+", Char: "+c+", Boolean: "+bool);
+  }
+}
