7c424e3d201abdea5051271311970eaef171a392
Philip Oliveira <mathphilips007@gmail.com>
2025-11-06 16:33:41 -0300

    refactor(java): Renomeia classe para PrimitiveTypes

--- PrimitiveTypes.java
+++ PrimitiveTypes.java
@@ -1,16 +1,17 @@
-public class TiposPrimitivos {
+public class PrimitiveTypes {
   public static void main(String[] args) {
-    //int, double, float, char, byte, short, long, boolean, String 
+    // int, double, float, char, byte, short, long, boolean, String
     byte b = 1;
     short s = 1;
     int i = 1;
     long l = 1L;
     float f = 1.1f;
     double d = 1.1;
-    String str = "Não.";
+    String str = "No.";
     char c = 'N';
     boolean bool = true;
 
-    System.out.println("Byte: "+b+", Short: "+s+", Integer: "+i+", Long: "+l+", Float: "+f+", Double: "+d+", String: "+str+", Char: "+c+", Boolean: "+bool);
+    System.out.println("Byte: " + b + ", Short: " + s + ", Integer: " + i + ", Long: " + l + ", Float: " + f
+        + ", Double: " + d + ", String: " + str + ", Char: " + c + ", Boolean: " + bool);
   }
 }
